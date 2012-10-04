7509be6d9221f8b7f9acb1e6b592d1c96105fe92
Mark Panaghiston <markp@happyworm.com>
2012-10-04 01:20:48 +0100

    First draft of working animated pie chart.

--- js/d3.sandbox.js
+++ js/d3.sandbox.js
@@ -1,31 +1,50 @@
 $(document).ready(function(){   
 
-	function drawPieChart(data) {
+	var pie, arc, arcs, arcLabels, pie_dur, r;
+
+	function initPieChart() {
+
 		$('#pie-chart').empty();
+
+		// var data = [{"label":"-", "value":1},{"label":"-", "value":1}];
+
+		var data = [1,1]; // [dem, rep]
+		var labels = ['Dem','Rep'];
+
 		var w = 120, //width
 			h = 120, //height
-			r = 60, //radius
-			color = d3.scale.category20c(); //builtin range of colors
+			// color = d3.scale.category20c(); // builtin range of colors
+			color = ["#3366cc","#dc3912"]; // Changed to define an array. Usually this is a function!
 
-		color = ["#3366cc","#dc3912"];
+		r = 60; //radius
+		pie_dur = 2000; // 750; // ms
+		pie = d3.layout.pie().sort(null);
+		arc = d3.svg.arc().outerRadius(r); //this will create <path> elements for us using arc data
 
-		var vis = d3.select("#pie-chart")
+		var svg = d3.select("#pie-chart")
 			.append("svg:svg") //create the SVG element inside the <body>
-			.data([data]) //associate our data with the document
+			// .data([data]) //associate our data with the document
 			.attr("width", w) //set the width and height of our visualization (these will be attributes of the <svg> tag
-			.attr("height", h)
-			.append("svg:g") //make a group to hold our pie chart
-			.attr("transform", "translate(" + r + "," + r + ")") //move the center of the pie chart from 0, 0 to radius, radius
+			.attr("height", h);
+			// .append("svg:g") //make a group to hold our pie chart
+			// .attr("transform", "translate(" + r + "," + r + ")") //move the center of the pie chart from 0, 0 to radius, radius
 
-		var arc = d3.svg.arc() //this will create <path> elements for us using arc data
-			.outerRadius(r);
+		var arc_grp = svg.append("svg:g")
+			.attr("class", "arcGrp")
+			.attr("transform", "translate(" + r + "," + r + ")"); //move the center of the pie chart from 0, 0 to radius, radius
 
-		var pie = d3.layout.pie() //this will create arc data for us given a list of values
+		var label_grp = svg.append("svg:g")
+			.attr("class", "labelGrp")
+			.attr("transform", "translate(" + r + "," + r + ")"); //move the center of the pie chart from 0, 0 to radius, radius
+
+		
+/*
+		pie = d3.layout.pie() //this will create arc data for us given a list of values
 			.value(function(d) { return d.value; }); // we must tell it out to access the value of each element in our data array
 
 		pie.sort(null);
 
-		var arcs = vis.selectAll("g.slice") //this selects all <g> elements with class slice (there aren't any yet)
+		arcs = svg.selectAll("g.slice") //this selects all <g> elements with class slice (there aren't any yet)
 			.data(pie) //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
 			.enter() //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
 			.append("svg:g") //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
@@ -34,7 +53,8 @@ $(document).ready(function(){
 		arcs.append("svg:path")
 			.attr("fill", function(d, i) { return color[i]; } ) //set the color for each slice to be chosen from the color function defined above
 			.attr('stroke', '#fff')
-			.attr("d", arc); //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+			.attr("d", arc) //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+			.each(function(d) {this._current = d});
 
 		arcs.append("svg:text")  
 			.attr("fill","#fff") //add a label to each slice
@@ -46,13 +66,49 @@ $(document).ready(function(){
 			})
 			.attr("text-anchor", "middle") //center the text on it's origin
 			.text(function(d, i) { return data[i].label; }); //get the label from our original data array
+*/
+
+		// DRAW ARC PATHS
+		arcs = arc_grp.selectAll("path")
+			.data(pie(data));
+		arcs.enter().append("svg:path")
+			.attr("stroke", "white")
+			// .attr("stroke-width", 0.5)
+			.attr("fill", function(d, i) {return color[i];}) // Note using an array and not usual color(i) functions.
+			.attr("d", arc)
+			.each(function(d) {this._current = d});
+
+		// DRAW SLICE LABELS
+		arcLabels = label_grp.selectAll("text")
+			.data(pie(data));
+		arcLabels.enter().append("svg:text")
+			.attr("class", "arcLabel")
+			.attr("transform", function(d) {
+				d.innerRadius = 0;
+				d.outerRadius = r;
+				return "translate(" + arc.centroid(d) + ")";
+			})
+			.attr("text-anchor", "middle")
+			.text(function(d, i) {return labels[i]; });
 	}
 
-	function pieChart(repCount, demCount) {
+	initPieChart();
+
+	// Store the currently-displayed angles in this._current.
+	// Then, interpolate from this._current to the new angles.
+	function arcTween(a) {
+		var i = d3.interpolate(this._current, a);
+		this._current = i(0);
+		return function(t) {
+			return arc(i(t));
+		};
+	}
+
+	function updatePieChart(repCount, demCount) {
 
 		var totCount = repCount + demCount,
-			rPc = Math.round((repCount/(totCount)*1000))/10,
-			dPc = Math.round((demCount/(totCount)*1000))/10;
+			rPc = Math.round(repCount/totCount*1000)/10,
+			dPc = Math.round(demCount/totCount*1000)/10;
 
 		if (repCount == 0) {
 			dPc = "";
@@ -67,26 +123,39 @@ $(document).ready(function(){
 		}
 
 		var piedata = [{"label":dPc, "value":demCount},{"label":rPc, "value":repCount}];
+		var data = [demCount,repCount];
+		var labels = [dPc,rPc];
 
-		drawPieChart(piedata);
+		arcs.data(pie(data)); // recompute angles, rebind data
+		arcs.transition().ease("elastic").duration(pie_dur).attrTween("d", arcTween);
+
+		arcLabels.data(pie(data));
+		arcLabels.text(function(d, i) {return labels[i]; });
+		arcLabels.transition().ease("elastic").duration(pie_dur)
+			.attr("transform", function(d) {
+				d.innerRadius = 0;
+				d.outerRadius = r;
+				return "translate(" + arc.centroid(d) + ")";
+			})
+			.style("fill-opacity", function(d) {return d.value==0 ? 1e-6 : 1;});
 	}
 
 	// pieChart(10, 20);
 
 	$('#pie-a').click(function() {
-		pieChart(5, 10);
+		updatePieChart(5, 10);
 		return false;
-	}).click();
+	}); // .click();
 	$('#pie-b').click(function() {
-		pieChart(25, 40);
+		updatePieChart(25, 40);
 		return false;
 	});
 	$('#pie-c').click(function() {
-		pieChart(60, 50);
+		updatePieChart(60, 50);
 		return false;
 	});
 	$('#pie-d').click(function() {
-		pieChart(90, 15);
+		updatePieChart(90, 15);
 		return false;
 	});
 
